39333996d24e9ee09e25ab308f65b8b213dea8e7
Dang Quang Thang <e1129943@u.nus.edu>
2025-01-22 21:05:40 +0800

    Level-2: Add, List

--- src/main/java/MightyDuck.java
+++ src/main/java/MightyDuck.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class MightyDuck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
 
         System.out.println("""
                 ================================================================
@@ -15,16 +16,26 @@ public class MightyDuck {
             String command = sc.nextLine();
 
             if (command.equals("bye")) {
-                break;
-            }
-
-            System.out.println("\t" + command + "\n");
-        }
-
-        System.out.println("""
+                System.out.println("""
                     Farewell, citizens of Duckville!
                     This is Mighty Duck, flying off to new adventures!
                 ================================================================
                 """);
+                break;
+            }
+
+            if (command.equals("list")) {
+                int i = 0;
+                for (String task: tasks) {
+                    i++;
+                    System.out.println("\t" + i + ". " + task);
+                }
+            } else {
+                tasks.add(command);
+                System.out.println("\t" + "added: " + command);
+            }
+
+            System.out.println();
+        }
     }
 }
