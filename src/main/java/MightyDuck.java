4f53a252e02742bad800a4c03a67fef6c4b35a87
Dang Quang Thang <e1129943@u.nus.edu>
2025-01-22 20:48:29 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/MightyDuck.java
@@ -0,0 +1,14 @@
+public class MightyDuck {
+    public static void main(String[] args) {
+        System.out.println("""
+                    Greetings, citizens of Duckville! \
+                This is Mighty Duck!
+                    How can I serve you today?
+                """);
+
+        System.out.println("""
+                    Farewell, citizens of Duckville!
+                    This is Mighty Duck, flying off to new adventures!
+                """);
+    }
+}
