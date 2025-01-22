1d8720d1f9c698ca1493197f455f49760ee4c71a
Dang Quang Thang <e1129943@u.nus.edu>
2025-01-22 20:59:08 +0800

    Level-1: Echo

--- src/main/java/MightyDuck.java
+++ src/main/java/MightyDuck.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class MightyDuck {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("""
+                ================================================================
                     Greetings, citizens of Duckville! \
                 This is Mighty Duck!
                     How can I serve you today?
                 """);
 
+        while (true) {
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                break;
+            }
+
+            System.out.println("\t" + command + "\n");
+        }
+
         System.out.println("""
                     Farewell, citizens of Duckville!
                     This is Mighty Duck, flying off to new adventures!
+                ================================================================
                 """);
     }
 }
